035f94878949c1a02425379f78ce181a2881bd7a
see <upcwangying@126.com>
2022-08-28 14:44:44 +0800

    feat: initial commit

---
+++ packages/components/config/utils.js
@@ -0,0 +1,26 @@
+const glob = require('glob')
+
+function getEntries() {
+  let map = {}
+  const entryFiles = glob.sync('./src/**/index.js?(x)')
+  entryFiles.forEach((filepath) => {
+    let fileDir = /.\/src\/(.*?)\.jsx?/.exec(filepath)
+    map[fileDir[1]] = filepath
+  })
+  return map
+}
+
+function getUmdEntries() {
+  let map = {}
+  const entryFiles = glob.sync('./src/**/umd.js?(x)')
+  entryFiles.forEach((filepath) => {
+    let fileDir = /\.\/src\/(\w+)\/.*\.jsx?/.exec(filepath)
+    map[fileDir[1]] = filepath
+  })
+  return map
+}
+
+module.exports = {
+  getEntries,
+  getUmdEntries,
+}
